08be64fd38e191c2c204e7985bccd86752bce985
suryateja vemuri <suryateja vemuri>
2016-09-18 20:10:44 -0400

    first commit

---
+++ EmployeePay/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace EmployeePay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSSN = new System.Windows.Forms.TextBox();
+            this.txtBR = new System.Windows.Forms.TextBox();
+            this.txtHrs = new System.Windows.Forms.TextBox();
+            this.txtGross = new System.Windows.Forms.TextBox();
+            this.txtFed = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtNet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMPLOYEE PAYROLL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SSN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Billing Rate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Hours Worked";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gross Pay";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Federal Tax";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "State Tax";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Net Pay";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(98, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(155, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtSSN
+            // 
+            this.txtSSN.Location = new System.Drawing.Point(98, 69);
+            this.txtSSN.MaxLength = 9;
+            this.txtSSN.Name = "txtSSN";
+            this.txtSSN.Size = new System.Drawing.Size(100, 20);
+            this.txtSSN.TabIndex = 10;
+            // 
+            // txtBR
+            // 
+            this.txtBR.Location = new System.Drawing.Point(98, 96);
+            this.txtBR.Name = "txtBR";
+            this.txtBR.Size = new System.Drawing.Size(50, 20);
+            this.txtBR.TabIndex = 11;
+            // 
+            // txtHrs
+            // 
+            this.txtHrs.Location = new System.Drawing.Point(98, 124);
+            this.txtHrs.Name = "txtHrs";
+            this.txtHrs.Size = new System.Drawing.Size(50, 20);
+            this.txtHrs.TabIndex = 12;
+            // 
+            // txtGross
+            // 
+            this.txtGross.Enabled = false;
+            this.txtGross.Location = new System.Drawing.Point(98, 167);
+            this.txtGross.Name = "txtGross";
+            this.txtGross.Size = new System.Drawing.Size(100, 20);
+            this.txtGross.TabIndex = 13;
+            // 
+            // txtFed
+            // 
+            this.txtFed.Enabled = false;
+            this.txtFed.Location = new System.Drawing.Point(98, 195);
+            this.txtFed.Name = "txtFed";
+            this.txtFed.Size = new System.Drawing.Size(100, 20);
+            this.txtFed.TabIndex = 14;
+            // 
+            // txtState
+            // 
+            this.txtState.Enabled = false;
+            this.txtState.Location = new System.Drawing.Point(98, 223);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(100, 20);
+            this.txtState.TabIndex = 15;
+            // 
+            // txtNet
+            // 
+            this.txtNet.Enabled = false;
+            this.txtNet.Location = new System.Drawing.Point(98, 252);
+            this.txtNet.Name = "txtNet";
+            this.txtNet.Size = new System.Drawing.Size(100, 20);
+            this.txtNet.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(258, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "NET PAY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 379);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNet);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtFed);
+            this.Controls.Add(this.txtGross);
+            this.Controls.Add(this.txtHrs);
+            this.Controls.Add(this.txtBR);
+            this.Controls.Add(this.txtSSN);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pay roll Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSSN;
+        private System.Windows.Forms.TextBox txtBR;
+        private System.Windows.Forms.TextBox txtHrs;
+        private System.Windows.Forms.TextBox txtGross;
+        private System.Windows.Forms.TextBox txtFed;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtNet;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EmployeePay/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeePay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double hours, billingRate, grossPay, federalTax, stateTax, netPay;
+            string name;
+            string ssn;
+
+
+            name = txtName.Text;
+            ssn = txtSSN.Text;
+            
+           
+            try
+            {
+                int ssnNumber = int.Parse(ssn);
+                if (name.Length != 0 && ssn.Length != 0)
+                {
+                    hours = Convert.ToDouble(txtHrs.Text);
+                    billingRate = Convert.ToDouble(txtBR.Text);
+
+                    grossPay = hours * billingRate;
+                    federalTax = grossPay * 0.15;
+                    stateTax = grossPay * 0.0765;
+                    netPay = grossPay - (federalTax + stateTax);
+
+                    txtGross.Text = grossPay.ToString("c");
+                    txtFed.Text = federalTax.ToString("c");
+                    txtState.Text = stateTax.ToString("c");
+                    txtNet.Text = netPay.ToString("c");
+                }
+                else
+                {
+                    MessageBox.Show("Name and SSN field should be a valid input");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ssn should be a numeric value");
+            }
+            
+        }
+    }
+}
